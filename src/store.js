0c6b50a2e728c1b9bc85ae4dc402a5fb45664895
evecomcom98 <evecomcom98@naver.com>
2023-05-18 13:10:59 +0900

    Feat: store 기본 세팅

--- src/store.js
+++ src/store.js
@@ -1,55 +1,86 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import { createVuexPersistedState } from 'vue-persistedstate'
+import Vue from "vue";
+import Vuex from "vuex";
+import { createVuexPersistedState } from "vue-persistedstate";
 
-Vue.use(Vuex)
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    books: [],
-    book: {
-      isbn: String,
-      title: String,
-      author: String,
-      price: Number,
-      content: String,
-    },
-  },
-  getters: {
-    booksLength(state) {
-      return state.books.length;
-    }
+    user: {},
+    loginUser: null,
   },
   mutations: {
-    SET_BOOKS(state, books) {
-      state.books = books;
+    CREATE_USER(state, user) {
+      state.users.push(user);
     },
-    SET_BOOK(state, book) {
-      state.book = book;
-    }
+    SET_USER(state, user) {
+      state.user = user;
+    },
+    SET_LOGIN_USER(state, user) {
+      state.loginUser = user;
+    },
+    LOGOUT(state) {
+      state.loginUser = null;
+    },
   },
   actions: {
-    getBooks({commit}) {
-      http.get("/book").then(({ data }) => {
-        commit("SET_BOOKS", data);
-      }).catch(() => {
-        alert("도서목록: 에러발생")
-      });
+    createUser({ commit }, user) {
+      http.post("/userapi/user", user)
+        .then(() => {
+          alert("등록되었습니다.");
+          router.push({name: "home"});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    getBook({commit}, isbn) {
-      http.get("/book/"+isbn).then(({ data }) => {
-        commit("SET_BOOKS", data);
-      }).catch(() => {
-        alert("도서조회: 에러발생")
-      });
-    }
+    updateUser({ commit }, user) {
+      http.put("/userapi/user", user)
+        .then(() => {
+          alert("수정 완료!");
+          router.push({name : "home"});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    deleteUser({ state }, id) {
+      console.log(state);
+      http.delete("/userapi/user/"+id)
+        .then(() => {
+          alert("삭제 완료!");
+          router.push({name: "home"});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    setUser({ commit }, id) {
+      http.get("/userapi/user/"+id)
+        .then(({ data }) => {
+          commit("SET_USER", data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    setLoginUser({ commit }, user) {
+      http.post("/userapi/login")
+        .then(({ data }) => {
+            commit("SET_LOGIN_USER", data);
+            router.push({name: "home"});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   plugins: [
     createVuexPersistedState({
-      //key : 'vuex',
-      storage: sessionStorage, //localStorage
-    })
+      //key: 'vuex', //기본값 'vuex'
+      storage: window.sessionStorage, //기본값 localStorage
+    }),
   ],
-  modules: {
-  }
-})
+  modules: { user},
+});
