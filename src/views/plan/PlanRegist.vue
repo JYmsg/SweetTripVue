22257c44e8b85c83a26c399278578ff973cf5646
evecomcom98 <evecomcom98@naver.com>
2023-05-21 21:26:14 +0900

    Feat: 일정과 검색 분리 완료

--- src/views/plan/PlanRegist.vue
+++ src/views/plan/PlanRegist.vue
@@ -11,11 +11,18 @@
         <!-- 계획표는 할당된 곳의 7칸 -->
         <div class="col-lg-7 col-sm-7 m-0">
           <div id="sidebar" class="p-3">
-            <div id="title_plan" class="text-center p-1">
-              <h4 class="mt-2">여행 계획표</h4>
-              <h5>{{travel.title}} : {{travel.startdate}} ~ {{travel.enddate}}</h5>
+            <div class="row no-gutters">
+              <div :id="Title[0]" class="text-center p-1 col-lg-6" @click="change(1)" >
+                <h4 class="mt-2" :id="Title[0]">여행 계획표</h4>
+              </div>
+              <div :id="Title[1]" class="text-center p-1 col-lg-6" @click="change(2)">
+                <h4 class="mt-2" :id="Title[1]">검색</h4>
+                <!-- <h5>분리중</h5> -->
+              </div>
             </div>
-            <div id="days" style="overflow:auto; max-height: 90vh;">
+            <plan-list v-if="select" :travel="travel" :colors="colors" @dumy="dumy" @onlyLine="onlyLine"></plan-list>
+            <plan-search v-else :travel="travel" :colors="colors" @dumy="dumy" @onlyLine="onlyLine"></plan-search>
+            <!-- <div id="days" style="overflow:auto; max-height: 90vh;">
               <div id="day" class="mb-1" v-for="(day, index) in travel.days" :key="day.id">
                 <h3 class="day pl-3 pt-1" :style="'color: ' + colors[index % 9]" @click="onlyLine(index)">Day{{index+1}}({{day.date}})</h3>
                 <div id="attractions" v-for="place in day.places" :key="place.content_id" class="p-1">
@@ -33,7 +40,7 @@
                   <base-button class="btn-2 p-1 pt-2" type="light" icon="ni ni-fat-add" style="box-shadow: none; width: 2rem; height: 2rem;" @click="dumy(index)"></base-button>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
         </div>
         <!-- 장바구니는 5칸 -->
@@ -90,9 +97,13 @@
 import Modal from "@/components/Modal.vue";
 import http from "@/util/http-common.js";
 import { mapState } from 'vuex';
+import PlanList from './element/PlanList.vue';
+import PlanSearch from './element/PlanSearch.vue';
 export default {
   components:{
     Modal,
+    PlanList,
+    PlanSearch,
   },
   data(){
     return{
@@ -101,12 +112,13 @@ export default {
       carts: [],
       cartslength: 0,
       modal: false,
-      moveLine: null,
       clickLines: [],
       testLine: null,
       distanceOverlays: [],
       dots : [],
       markers: [],
+      select: true,
+      Title: ["title_plan", "noTitle"],
       colors : ['#dc3545', '#fd7e14', '#ffc107', '#28a745', '#20c997', '#17a2b8', '#007bff', '#6610f2', '#6f42c1'], //9가지 색
     }
   },
@@ -151,6 +163,20 @@ export default {
     }
   },
   methods: {
+    change(index){
+      if(index == 1){
+        this.Title[1] = "noTitle";
+        this.Title[0] = "title_plan";
+        this.select = true;
+      }
+      else{
+        this.Title[0] = "noTitle";
+        this.Title[1] = "title_plan";
+        this.select = false;
+      }
+      this.initMap();
+
+    },
     initMap() {
       var container = document.getElementById("map");
       var options = {
@@ -158,7 +184,14 @@ export default {
           level: 8, // 지도의 확대 레벨
       };
       this.map = new kakao.maps.Map(container, options);
-      this.initLine();
+      if(this.select){
+        this.initLine();
+      }else{
+        this.testLine = null;
+        this.distanceOverlays = [];
+        this.dots = [];
+        this.markers = [];
+      }
     },
     makeMarkers(index){
       console.log(index);
@@ -468,6 +501,14 @@ export default {
 }
 #title_plan{
   background-color: #C4DFDF;
+  height: 5.5%;
+  border-radius: 20px 20px 0px 0px;
+}
+#noTitle{
+  background-color: black;
+  color: white;
+  height: 5.5%;
+  border-radius: 20px 20px 0px 0px;
 }
 .day{
   background-color: #D2E9E9;
---
+++ src/views/plan/element/PlanSearch.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+    <div id="date" class="p-2 pb-0">
+        <h5>{{travel.title}} : {{travel.startdate}} ~ {{travel.enddate}}</h5>
+    </div>
+    <div id="days" style="overflow:auto; max-height: 90vh;" class="mt-0">
+        <div id="day" class="mb-1" v-for="(day, index) in travel.days" :key="day.id">
+        <h3 class="day pl-3 pt-1" :style="'color: ' + colors[index % 9]" @click="onlyLine(index)">Day{{index+1}}({{day.date}})</h3>
+        <div id="attractions" v-for="place in day.places" :key="place.content_id" class="p-1">
+            <div v-if="place">
+            <div id="att_img_box" class="mb-1">
+                <img id="att_img" :src="`${place.first_image}`" alt="">
+            </div>
+            <div id="att_address_box" class="text-center mb-1">
+                <h4 class="mt-2 mb-0">{{ place.title }}</h4>
+                <p>{{place.addr1}}</p>
+            </div>
+            </div>
+        </div>      
+        <div class="text-center pb-2 mt-2">
+            <base-button class="btn-2 p-1 pt-2" type="light" icon="ni ni-fat-add" style="box-shadow: none; width: 2rem; height: 2rem;" @click="dumy(index)"></base-button>
+        </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props: [
+      'travel',
+      'colors'
+    ],
+    methods:{
+        dumy(index){
+            this.$emit("dumy", index);
+        },
+        onlyLine(index){
+            this.$emit("onlyLine", index);
+        }
+    },
+}
+</script>
+<style>
+#date{
+    background-color: #C4DFDF;
+}
+#sidebar{
+  width: 100%;
+  height: 100%;
+  background-color: #F8F6F4;
+}
+#title_plan{
+  background-color: #C4DFDF;
+}
+.day{
+  background-color: #D2E9E9;
+}
+#days{
+  background-color: #E3F4F4;
+}
+#attractions{
+  width: 100%;
+  height: 6.5rem;
+  /* background-color: #A5D7E8; */
+  background-color: black;
+}
+#att_img_box{
+  float: left;
+  width: 33%;
+  height: 6rem;
+}
+#att_address_box{
+  float: right;
+  width: 66%;
+  height: 6rem;
+  background: #F8F6F4;
+  overflow: hidden;
+}
+</style>
